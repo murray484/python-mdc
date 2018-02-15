3b96de848ade2bf12b6eda66dc29b6cd3aed0e15
Damien PLENARD <dplenard@vente-privee.com>
2018-02-15 15:23:08 +0100

    create python module with setup.py

---
+++ mdc/__main__.py
@@ -0,0 +1,9 @@
+"""Provide a command line tool to send MDC command."""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+
+setuptools.setup(
+    name="mdc",
+    version="0.1.0",
+    description="",
+    url="https://git.vpgrp.io/noc/python-mdc",
+    author="Damien PLENARD",
+    author_email="dplenard@vente-privee.com",
+    license="Apache License 2.0",
+    long_description=open("README.md").read(),
+    entry_points={
+        "console_scripts": ["mdc=mdc.__main__:main"]
+    },
+    packages=["mdc"],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Environment :: Console",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Home Automation",
+        "Topic :: Multimedia :: Video :: Display"
+    ],
+)
